81c3ddcbe2346337a1dbfbf8c7d0c5dc46ecaac3
asura <saygram@gmail.com>
2017-03-08 17:32:35 +0100

    Added comments for increased readability. Split into directories so more examples can be added.

--- 0_blinky/main.c
+++ 0_blinky/main.c
@@ -8,22 +8,24 @@ void delay(void);
 
 int main(void) {
     /* Enable clock to IO port C */
-    RCC->APB2ENR = RCC_APB2ENR_IOPCEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
     /* Set pin C13 as an output */
-    GPIOC->CRH &= ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13);
-    GPIOC->CRH |= GPIO_CRH_MODE13;
+    GPIOC->CRH &= ~GPIO_CRH_MODE13; //Clear bits
+    GPIOC->CRH |= GPIO_CRH_MODE13;  //Output mode, max 50 MHz
+    GPIOC->CRH &= ~GPIO_CRH_CNF13;  //GPIO output push-pull
 
     while(1) {
-        GPIOC->BSRR = (1U << 13U);
+        GPIOC->BSRR = (1U << 13U); //Set pin HIGH
         delay();
-        GPIOC->BRR = (1U << 13U);
+        GPIOC->BRR = (1U << 13U); //Reset pin to LOW
         delay();
     }
 }
 
+/* Primitive delay function */
 void delay(void) {
-     uint8_t i; 
-     uint8_t j;
+    uint8_t i; 
+    uint8_t j;
 
     for (i = 0; i < 0xFF; i++) {
         for (j = 0; j < 0xFF; j++) { 
